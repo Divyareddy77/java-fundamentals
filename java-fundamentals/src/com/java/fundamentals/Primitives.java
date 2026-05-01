28e65a636a430607d361ea20198b8157e415011c
Divyareddy77 <vangadivyareddy1210@gmail.com>
2026-05-01 14:52:54 +0530

    "updated primitives"

--- java-fundamentals/src/com/java/fundamentals/Primitives.java
+++ java-fundamentals/src/com/java/fundamentals/Primitives.java
@@ -20,11 +20,40 @@ public class Primitives {
         System.out.println("Hindi marks : "+hindiMarks);
         System.out.println("English marks : "+englishMarka);
         System.out.println("Maths marks : "+mathsMarks);
+        //other 3 variables
+        byte rating;
+        rating=4;
+        System.out.println("Rating is " +rating);
+
+        byte heartRate;
+        heartRate=72;
+        System.out.println("Heart rate is "+heartRate);
+
+        byte attendance;
+        attendance=90;
+        System.out.println("Attendance is "+attendance);
+
+
+
 
 
         short  totalMarks;
         totalMarks=500;
         System.out.println("Total marks : "+totalMarks);
+
+        short speed;
+        speed=180;
+        System.out.println("Speed is "+speed +" km/hr");
+
+        short temperature;
+        temperature=200;
+        System.out.println("Temperature is "+temperature);
+
+        short schoolStrength;
+        schoolStrength=5000;
+        System.out.println("school strength is "+schoolStrength);
+
+
 
 
 
@@ -35,11 +64,34 @@ public class Primitives {
         System.out.println("salary is "+salary);
         System.out.println("product price is "+productPrice);
 
+        int population;
+        population=2099877;
+        System.out.println("population in a town is "+population);
+
+        int bankbalance;
+        bankbalance=234987;
+        System.out.println("BAnk balance is "+bankbalance);
+
+        int passengers;
+        passengers=300000;
+        System.out.println("number of passengers "+passengers);
+
+
+
 
 
         long companyAnnualRevenue;
         companyAnnualRevenue=1000000000000000L;
         System.out.println("Company annual revenue is "+ companyAnnualRevenue);
+
+        long worldPopulation;
+        worldPopulation=8000000000000L;
+        System.out.println("World population is "+worldPopulation);
+
+        long nationBudget;
+        nationBudget=200000000L;
+        System.out.println("Nation's budget is "+nationBudget);
+
 
 
 
@@ -48,10 +100,37 @@ public class Primitives {
         percentage=40.7F;
         System.out.println("percentage is "+percentage);
 
+        float weight;
+        weight=78.9F;
+        System.out.println("Weight is "+weight);
+
+        float height;
+        height=.8F;
+        System.out.println("Height is "+height);
+
+        float tax;
+        tax=5.5F;
+        System.out.println("Tax is "+tax);
+
+
+
+
 
         double totalPrice;
         totalPrice=38774.9;
         System.out.println("total price is "+totalPrice);
+
+        double pi;
+        pi=3.14159;
+        System.out.println("pi value is "+pi);
+
+        double accuracy;
+        accuracy=0.98646;
+        System.out.println("Accuracy is "+accuracy);
+
+        double frequency;
+        frequency=2.856557;
+        System.out.println("Frequency is "+frequency);
 
 
 
@@ -59,17 +138,35 @@ public class Primitives {
         gender='M';
         System.out.println("Gender : "+gender);
 
-
-
         char ch;
         ch=97;
         System.out.println("char "+ch);
+
+        char grade;
+        grade='A';
+        System.out.println("Grade is"+grade);
+
+        char section;
+        section='C';
+        System.out.println("Section is "+section);
+
+        char bloodGroup;
+        bloodGroup='B';
+        System.out.println("Blood group is "+bloodGroup);
 
 
 
         boolean isActive;
         isActive=true;
         System.out.println("IsActive : "+isActive);
+
+        boolean isLoggedIn;
+        isLoggedIn=false;
+        System.out.println("Is logged in"+isLoggedIn);
+
+        boolean isSuccessful;
+        isSuccessful=true;
+        System.out.println("Is successful "+isSuccessful);
 
 
 
