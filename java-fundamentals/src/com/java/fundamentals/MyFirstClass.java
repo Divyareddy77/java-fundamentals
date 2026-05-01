8cc695559809ec8dcb84319de893111935115e8a
Divyareddy77 <vangadivyareddy1210@gmail.com>
2026-05-01 14:10:09 +0530

    "updated primitives"

--- java-fundamentals/src/com/java/fundamentals/MyFirstClass.java
+++ java-fundamentals/src/com/java/fundamentals/MyFirstClass.java
@@ -3,5 +3,6 @@ package com.java.fundamentals;
 public class MyFirstClass {
     public static void main(String[] args) {
         System.out.println("This is my first java class");
+        System.out.println("Hello");
     }
 }
---
+++ java-fundamentals/src/com/java/fundamentals/Primitives.java
@@ -0,0 +1,78 @@
+package com.java.fundamentals;
+
+public class Primitives {
+    public static void main(String[] args) {
+        /*
+        * Primitives examples
+        * byte variable declaration
+        * */
+        byte age;//just a variable declaration
+        age=30;
+        byte screenSize=14;
+        System.out.println("Age : " + age);
+        System.out.println("Screen size " + screenSize + " inches");
+        byte teluguMarks, englishMarka, mathsMarks,hindiMarks;
+        teluguMarks=90;
+        hindiMarks=89;
+        englishMarka=97;
+        mathsMarks=78;
+        System.out.println("Teugu marks : "+teluguMarks);
+        System.out.println("Hindi marks : "+hindiMarks);
+        System.out.println("English marks : "+englishMarka);
+        System.out.println("Maths marks : "+mathsMarks);
+
+
+        short  totalMarks;
+        totalMarks=500;
+        System.out.println("Total marks : "+totalMarks);
+
+
+
+        int salary;
+        int productPrice;
+        salary=50000;
+        productPrice=90000;
+        System.out.println("salary is "+salary);
+        System.out.println("product price is "+productPrice);
+
+
+
+        long companyAnnualRevenue;
+        companyAnnualRevenue=1000000000000000L;
+        System.out.println("Company annual revenue is "+ companyAnnualRevenue);
+
+
+
+
+        float percentage;
+        percentage=40.7F;
+        System.out.println("percentage is "+percentage);
+
+
+        double totalPrice;
+        totalPrice=38774.9;
+        System.out.println("total price is "+totalPrice);
+
+
+
+        char gender;
+        gender='M';
+        System.out.println("Gender : "+gender);
+
+
+
+        char ch;
+        ch=97;
+        System.out.println("char "+ch);
+
+
+
+        boolean isActive;
+        isActive=true;
+        System.out.println("IsActive : "+isActive);
+
+
+
+
+    }
+}
